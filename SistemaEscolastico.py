11266d70986f021899a402eb8a8f178c3c5c0b1e
José Ignacio Yánez <joseignacioyanez@hotmail.com>
2022-07-04 16:11:07 -0500

    Copiar de Grupal e ignore

--- SistemaEscolastico.py
+++ SistemaEscolastico.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#Hola, este es un comentario.
+
 @app.route("/")
 def hello_world():
+    #Aquí necesitamos cambiar y poner más funciones.
     return render_template('ABP.html')
